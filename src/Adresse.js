2e7b71841639c7d18176a8e7bac14736abd190c1
houssem-mbarki <hassou23955@gmail.com>
2021-10-13 02:17:08 +0100

    first commit

---
+++ src/Adresse.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Adresse() {
+  return (
+    <div>
+      <h1>
+        26 Rue nakhil afh borj cedria ben arous <br /> email address: hassou23955@hotmail.com
+      </h1>
+    </div>
+  );
+}
+
+export default Adresse;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Adresse from "./Adresse";
+import "./App.css";
+import FullName from "./Component/Profile/FullName";
+import ProfilePhoto from "./Component/Profile/ProfilePhoto";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="a">
+      <FullName />
+      <ProfilePhoto />
+
+      <Adresse />
     </div>
   );
 }
